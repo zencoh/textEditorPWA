1fa4bd4ce98cf4d37acf63ba4f5667c07642b9ff
Cole Hartkopp <chsoccer4@outlook.com>
2023-04-25 13:02:15 -0500

    install event handler needs work

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,7 +2,23 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    const stashPrompt = event;
+    const installButton = document.getElementById('install-button');
+    installButton.addEventListener('click', () => {
+        stashPrompt.prompt();
+        stashPrompt.choice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('user installed prompt');
+            } else {
+                console.log('user declined install prompt')
+            }
+            // reset stashed prompt to null
+            stashPrompt = null;
+        })
+    })
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {});

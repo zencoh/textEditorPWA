a60ca8731fe0b44fb98d7a08ffdfd84bd270a0ef
Cole Hartkopp <chsoccer4@outlook.com>
2023-04-25 12:48:30 -0500

    database get and put route done

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,25 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not impemented');
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  await store.put({ data: content});
+  await tx.done;
+  console.log('Content added to database:')
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const allData = await store.getAll();
+  await tx.done;
+  console.log('All content retrieved', allData);
+  return allData;
+};
 
 initdb();

90267dbb21370c141b226ba27dd613e9bda2a3f5
Cole Hartkopp <chsoccer4@outlook.com>
2023-04-26 18:35:21 -0500

    removing starter code TODO comments

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,6 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('PUT to database');
   const db = await initdb();
@@ -23,7 +22,6 @@ export const putDb = async (content) => {
   console.log('Content added to database:', result.value);
 }
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from database');
   const db = await initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,7 +1,6 @@
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+
 window.addEventListener('beforeinstallprompt', (event) => {
     // Store the triggered events
     window.deferredPrompt = event;
@@ -9,25 +8,19 @@ window.addEventListener('beforeinstallprompt', (event) => {
     butInstall.classList.toggle('hidden', false);
   });
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
-
   if (!promptEvent) {
    return;
   }
-
   // Show prompt
   promptEvent.prompt();
-  
   // Reset the deferred prompt variable, it can only be used once.
   window.deferredPrompt = null;
-  
   butInstall.classList.toggle('hidden', true);
 
 });
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     // Clear prompt
   window.deferredPrompt = null;
